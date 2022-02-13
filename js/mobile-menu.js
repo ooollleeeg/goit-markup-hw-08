5e7e165c040bf9228c018f6eb5aa5e459a8ee674
Oleh Honchar <ooollleeeg@gmail.com>
2022-02-13 04:29:10 +0200

    исправляет мобильное меню, футер, хедер и каталог

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector('[data-Menu-open]'),
     closeMenuBtn: document.querySelector('[data-Menu-close]'),
     Menu: document.querySelector('[data-Menu]'),
+    body: document.querySelector('body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.Menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
